2cef83fe2805b48939fe4e498c4e2c7c9734f2a3
carlos fuentes <eduardofuentesingmeca@gmail.com>
2023-04-04 14:51:11 -0400

    refactor: home components

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
-import { MenuCategoryComponent } from './pages/menu-category/menu-category.component';
-import { ProductListComponent } from './pages/product-list/product-list.component';
-import { PromotionsComponent } from './pages/promotions/promotions.component';
-import { ProductsRecomendedComponent } from './pages/products-recomended/products-recomended.component';
+import { MenuCategoryComponent } from './components/menu-category/menu-category.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { PromotionsComponent } from './components/promotions/promotions.component';
+import { ProductsRecomendedComponent } from './components/products-recomended/products-recomended.component';
 
 
 @NgModule({
